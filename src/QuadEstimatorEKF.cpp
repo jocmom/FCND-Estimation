0e2ad4e5c31c6ad961575df8c413657770a8177c
jocmom <jombi@web.de>
2018-07-05 11:45:14 +0200

    ADD: nonlinear complementary filter implementation for attitude

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -93,9 +93,18 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  // float predictedPitch = pitchEst + dtIMU * gyro.y;
+  // float predictedRoll = rollEst + dtIMU * gyro.x;
+  // ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  
+  // The Quaternion<float> class has a handy FromEuler123_RPY function for creating a quaternion from Euler Roll/PitchYaw
+  Quaternion<float> q = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+  // Quaternion<float> also has a IntegrateBodyRate function, though this uses quaternions, not Euler angles 
+  q.IntegrateBodyRate(gyro, dtIMU);
+    
+  float predictedPitch = q.Pitch();
+  float predictedRoll = q.Roll();
+  ekfState(6) = q.Yaw();
 
   // normalize yaw to -pi .. pi
   if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
